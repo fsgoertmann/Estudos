1f57a29266a88ebf0be7859c1b2e714a42b98070
Felipe Souza Goertmann <felipe.goertmann@philips.com>
2023-03-09 13:49:57 -0300

    Navigation structure - React Router

---
+++ Udemy/Curso React/navegacao/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './views/App';
+
+ReactDOM.render(
+  <h1>
+    <App/>
+  </h1>,
+  document.getElementById('root')
+)
