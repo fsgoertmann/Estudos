76e1087d85f28a19422f70e4186e04b8a6ac7452
Felipe Souza Goertmann <felipe.goertmann@philips.com>
2023-03-10 09:05:00 -0300

    useState and useEffect

--- Udemy/Curso React/hooks/src/views/examples/UseEffect.jsx
+++ Udemy/Curso React/hooks/src/views/examples/UseEffect.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useEffect, useState } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+
+function calcFatorial (num) {
+    const value = parseInt(num);
+    if(value < 0) return -1;
+    if(value === 0) return 1;
+
+    return calcFatorial(value -1) * value;
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = useState(1);
+    const [fatorial, setFatorial] = useState(1);
+
+    useEffect(function() {
+        setFatorial(calcFatorial(number));
+    }, [number]);
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+            <div className="center">
+                <div>
+                    <span className="text">Fatorial:</span>
+                    <span className="text red">{ fatorial }</span>
+                </div>
+                <input type="number" className="input"
+                    value={number} onChange={e => setNumber(e.target.value)} />
+            </div>
         </div>
-    )
+    );
 }
 
-export default UseEffect
+export default UseEffect;
