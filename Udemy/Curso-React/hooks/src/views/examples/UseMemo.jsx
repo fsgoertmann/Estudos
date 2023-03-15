2c5bfe6914dcd92c4e55de9ec632082771599114
Felipe Souza Goertmann <felipe.goertmann@philips.com>
2023-03-15 13:35:07 -0300

    Memo and CallBack

--- Udemy/Curso-React/hooks/src/views/examples/UseMemo.jsx
+++ Udemy/Curso-React/hooks/src/views/examples/UseMemo.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const UseMemo = (props) => {
-    return (
-        <div className="UseMemo">
-            <PageTitle
-                title="Hook UseMemo"
-                subtitle="Retorna um valor memoizado!"
-            />
-        </div>
-    )
+function sum(a, b) {
+  const future = Date.now() + 2000;
+  while (Date.now() < future) { }
+  console.log(a+b)
+  return a + b
+}
+
+const UseMemo = props => {
+  const [v1, setV1] = useState(0);
+  const [v2, setV2] = useState(0);
+  const [v3, setV3] = useState(0);
+
+  const result = useMemo(() => sum(v1, v2), [v1, v2])
+
+  return (
+    <div className="UseMemo">
+      <PageTitle
+        title="Hook UseMemo"
+        subtitle="Retorna um valor memoizado!"
+      />
+      <div className="center">
+        <input type="number" className="input"
+          value={v1} onChange={e => { setV1(parseInt(e.target.value)) }} />
+        <input type="number" className="input"
+          value={v2} onChange={e => { setV2(parseInt(e.target.value)) }} />
+        <input type="number" className="input"
+          value={v3} onChange={e => { setV3(parseInt(e.target.value)) }} />
+        <span className="text">{result}</span>
+      </div>
+    </div>
+  )
 }
 
 export default UseMemo
