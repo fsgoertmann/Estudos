7ee22ab85615ac746ea060f67139d09a0858ee2d
Felipe Souza Goertmann <felipe.goertmann@philips.com>
2023-03-15 16:26:20 -0300

    Context and Reducer

--- Udemy/Curso-React/hooks/src/views/examples/UseContext.jsx
+++ Udemy/Curso-React/hooks/src/views/examples/UseContext.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
+import DataContext from '../../data/DataContext'
+import SectionTitle from '../../components/layout/SectionTitle';
+import { AppContext } from '../../data/Store';
+
 const UseContext = (props) => {
-    return (
-        <div className="UseContext">
-            <PageTitle
-                title="Hook UseContext"
-                subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
-            />
+
+  const { state, setState } = useContext(DataContext)
+
+  function incNumber(delta) {
+    setState({
+      ...state,
+      number: state.number + delta
+    })
+  }
+
+  const { number, setNumber } = useContext(AppContext)
+  const { text, setText } = useContext(AppContext)
+
+  useEffect(function () {
+    if (number > 4400) {
+      setText('Over five thousand!!!')
+    } else {
+      setText('Context API + Hooks')
+    }
+  }, [number])
+
+  return (
+    <div className="UseContext">
+      <PageTitle
+        title="Hook UseContext"
+        subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
+      />
+      <SectionTitle title="Exercício #01"></SectionTitle>
+      <div className="center">
+        <button className="btn"
+          onClick={() => { incNumber(-1) }}>-1</button>
+        <button className="btn"
+          onClick={() => { incNumber(1) }}>+1</button>
+        <span className="text">{state.text}</span>
+        <span className="text">{state.number}</span>
+      </div>
+      <SectionTitle title="Exercício #02"></SectionTitle>
+      <div className="center">
+        <span className="text">{text}</span>
+        <span className="text">{number}</span>
+        <div>
+          <button className="btn"
+            onClick={() => setNumber(number - 1)}>-1</button>
+          <button className="btn"
+            onClick={() => setNumber(number + 1)}>+1</button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default UseContext
--- Udemy/Curso-React/hooks/src/views/examples/UseReducer.jsx
+++ Udemy/Curso-React/hooks/src/views/examples/UseReducer.jsx
@@ -1,15 +1,74 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+const initialState = {
+  cart: [],
+  products: [],
+  user: null,
+  number: 0
+}
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'numberAdd2':
+      return { ...state, number: state.number + 2 }
+    case 'login':
+      return {
+        ...state, user: { name: action.payload }
+      }
+    case 'updateNumber':
+      return {
+        ...state, number: action.payload.value
+      }
+    case 'multiply':
+      return {
+        ...state, number: state.number * action.payload.value
+      }
+    case 'divide':
+      return {
+        ...state, number: state.number / action.payload.value
+      }
+    case 'parseInt':
+      return {
+        ...state, number: parseInt(state.number)
+      }
+    default:
+      return state
+  }
+}
 
 const UseReducer = (props) => {
-    return (
-        <div className="UseReducer">
-            <PageTitle
-                title="Hook UseReducer"
-                subtitle="Uma outra forma de ter estado em componentes funcionais!"
-            />
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return (
+    <div className="UseReducer">
+      <PageTitle
+        title="Hook UseReducer"
+        subtitle="Uma outra forma de ter estado em componentes funcionais!"
+      />
+      <SectionTitle title='Exercício #01'></SectionTitle>
+      <div className="center">
+        {state.user ?
+          <span className="text">{state.user.name}</span> :
+          <span className="text">Sem usuário</span>
+        }
+        <span className="text">{state.number}</span>
+        <div>
+          <button className="btn"
+            onClick={() => { dispatch({ type: 'numberAdd2' }) }}>+2</button>
+          <button className="btn"
+            onClick={() => { dispatch({ type: 'login', payload: 'Feumann' }) }}>Login</button>
+          <button className="btn"
+            onClick={() => { dispatch({ type: 'multiply', payload: { value: 7 } }) }}>Vezes 7</button>
+          <button className="btn"
+            onClick={() => { dispatch({ type: 'divide', payload: { value: 25 } }) }}>Divide 25</button>
+          <button className="btn"
+            onClick={() => { dispatch({ type: 'parseInt' }) }}>Arredondar</button>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default UseReducer
